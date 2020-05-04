8c658d0c7be18a655aa94b804e99e2ed58d2bda5
Unknown <lorenzo.palumbo@hotmail.com>
2020-05-04 20:12:37 +0200

    commit 1

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <a href="#">Home</a>
+        <a href="{{route('prodotti')}}">Prodotti</a>
+        <a href="#">News</a>
+    </body>
+</html>
